4fdcefeb9bd1df2640d0ab52448f8cfca9772f59
nooroongy <haerammy@gmail.com>
2022-01-06 02:04:18 +0900

    test

--- src/App.js
+++ src/App.js
@@ -4,15 +4,20 @@ import Block from "./components/Block";
 function App() {
   return (
     <div className="App">
-      <Block contesnts={
-        <>
-          <NooButton w='4' h='5' >클릭</NooButton>
-          <NooButton w='5' h='5' >클릭</NooButton>
-          <NooButton w='6' h='5' >클릭</NooButton>
-          <NooButton w='7' h='5' >클릭</NooButton>
-          <NooButton w='8' h='5' >클릭</NooButton>
-          <NooButton w='9' h='5' >클릭</NooButton>
-          <NooInput></NooInput>
+      <Block row={[1, 8, 1]} col={[1, 4, 1]} contesnts={
+        <><div></div><div></div><div></div><div></div>
+          <Block contesnts={
+            <>
+              <NooButton >클릭</NooButton>
+              <NooButton >클릭</NooButton>
+              <NooButton >클릭</NooButton>
+              <NooButton >클릭</NooButton>
+              <NooButton >클릭</NooButton>
+              <NooButton >클릭</NooButton>
+              <NooInput></NooInput>
+            </>
+          }>
+          </Block>
         </>
       } />
     </div>
--- src/components/Block.js
+++ src/components/Block.js
@@ -2,10 +2,15 @@
 import React from 'react';
 import '../css/block.css'
 
-const Block = ({ contesnts }) => {
+const Block = ({ contesnts,row=[1],col=[1] }) => {
+    const RowTemp = row.map(r=>r+'fr').join(' ');
+    const colTemp = row.map(r=>r+'fr').join(' ');
 
     return (
-        <div className='nooroongy-block' style={{  }}>
+        <div className='nooroongy-block' style={{ 
+            gridTemplateRows: RowTemp,
+            gridTemplateColumns: colTemp,
+         }}>
             {contesnts}
         </div>
     )
